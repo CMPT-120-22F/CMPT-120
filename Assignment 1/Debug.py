1f6784614825399d8c3118dcee4a5966384bdeb1
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-06-02 23:38:57 -0400

    Initial Push

---
+++ Assignment 1/Debug.py
@@ -0,0 +1,26 @@
+#Assignment 1: Debug.py
+#Fix any syntax errors you can spot and fulfill any other requests
+def main():
+    print(hello)
+    print(3)
+    print(10 + 10)
+
+
+    #insert your name in this string below
+    print("My name is ")
+    #this is printing a string and not an integer. how do we fix it?
+    print("50")
+    #make sure this one prints out the sum
+    print("2 + 2")
+
+    #insert your name into this variable
+    name = ""
+    print("Hello, my name is ", name)
+
+    #insert your best friend's name here and have it print out
+    name2 = ""
+    print("Hello, my best friend's name is ", name)
+
+
+
+main()
---
+++ Assignment 1/helloworld.py
@@ -0,0 +1,5 @@
+def main():
+    print(hello)
+    print(3)
+    #insert your name in this string below
+    print("My name is ")
