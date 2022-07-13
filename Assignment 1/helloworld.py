a39f84afbf4daa8762782976e57ea98262122634
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-07-13 13:22:38 -0400

    Update helloworld.py

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,7 +1,7 @@
-#instructions: create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
+#instructions: print out "Hello World!"
+#create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
 #create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
-#ask the user for their name and then print out "Your name is: " with the variable
-#ask the user for the number of pets they have then print out "You have: " variable " pets"
+
 
 
 def main():
