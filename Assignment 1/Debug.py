2ea771bade0fd30f4d52a0fb2809fbc54f93ef06
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-08-30 19:40:30 -0400

    Update Debug.py

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -1,31 +1,33 @@
+
 #Assignment 1: Debug.py
 #Fix any syntax errors you can spot and fulfill any other requests
 def main():
     print(hello)
-    int(3)
-    sum(10 + 10)
+    print(3)
+    print(10 + 10)
 
 
     #insert your name in this string below
-    print("My name is Connor")
+    print("My name is ")
     #this is printing a string and not an integer. how do we fix it?
-    int("50")
+    print("50")
     #make sure this one prints out the sum
-    sum("2 + 2")
+    print("2 + 2")
 
     #insert your name into this variable
-    name = "Connor"
+    name = ""
     print("Hello, my name is ", name)
 
     #insert your best friend's name here and have it print out
-    name2 = "Brian"
+    name2 = ""
     print("Hello, my best friend's name is ", name)
     
     #put your age here
-    age = 17
+    age = 
     print("I'm " + name + " and i'm " + str(age) + " years old")
     
  
    
 
 main()
+
