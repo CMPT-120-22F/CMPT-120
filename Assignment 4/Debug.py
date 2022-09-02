747e28b8a7c70290321776f3ff11d1d08ab9c163
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-09-02 08:24:17 -0400

    Create Debug.py

---
+++ Assignment 4/Debug.py
@@ -0,0 +1,14 @@
+def main():
+    var1= 10
+    var2= 20
+    #What do we put in here to make it work?
+    addition()
+
+
+
+def addition(x, y):
+    #add x and y together and return them
+    return
+
+
+main()
