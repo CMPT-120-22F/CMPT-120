04cddaeb3c772f13954e473ec36a8beeef58c2df
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-09-02 10:50:44 -0400

    Update if_else_practice.py

--- Assignment 2/if_else_practice.py
+++ Assignment 2/if_else_practice.py
@@ -1,5 +1,5 @@
 #Instructions: Create a variable of any name and set it equal to 10.
-#The first if statement should read: if 10 is greater than 12, print out "10 is greater than 12"
+#The first if statement should read: if 10 (but use our variable, not the number 10) is greater than 12, print out "10 is greater than 12"
 #The second if/else should read: Else if 10 is greater than 11, print out "10 is greater than 11"
 #The third if/else should read: Else if 10 is equal to 10, print out "10 is equal to 10"
 #The else should read: Else print out "10 is less than 10"
