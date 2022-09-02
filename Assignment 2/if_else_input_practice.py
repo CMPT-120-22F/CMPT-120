ce4e8f2d59576b02d0945ee40a1289906233ba07
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-09-02 11:01:39 -0400

    Update and rename if_else_input_practice to if_else_input_practice.py

---
+++ Assignment 2/if_else_input_practice.py
@@ -0,0 +1,13 @@
+
+#These instructions below are copied fromt the other example, the adjustments are a space after
+#Instructions: Create a variable of any name and set it equal to 10.
+#The first if statement should read: if 10 (but use our variable, not the number 10) is greater than 12, print out "10 is greater than 12"
+#The second if/else should read: Else if 10 is greater than 11, print out "10 is greater than 11"
+#The third if/else should read: Else if 10 is equal to 10, print out "10 is equal to 10"
+#The else should read: Else print out "10 is less than 10"
+
+
+#additional instructions: instead of setting our variable to 10, what if we asked the user for the variable and used that value in our comparisons?
+def main():
+
+main()
