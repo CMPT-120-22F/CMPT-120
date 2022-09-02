993f7d5b76f182c191d1a73af8995ef15afea402
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-09-02 10:59:57 -0400

    Update Debug.py

--- Assignment 2/Debug.py
+++ Assignment 2/Debug.py
@@ -1,8 +1,19 @@
 #instructions: Something about these if statements aren't giving the desired effect. Look at them and see how to fix them. (Run them yourself and see what the output is!)
-
+#also, we have some input practice!
 
 def main():
     
+    #this is a nice solid working one!
+    name = input("your name is? ")
+    print("Hello, ", name)
+
+    #this isn't printing anything :( so sad. what's wrong with her? Why she not printing out?
+    color = input("what's your favorite color? ")
+    
+    #I thought i did this right :( can you fix it for me?
+    age = input("how old are you? ")
+    print("You are " + age + " years old")
+	
     #Start with this one! We have a compilation error :(
     #Side note: you can put these statements in parentheses or not. it's up to you.
     if (5 > 3)
