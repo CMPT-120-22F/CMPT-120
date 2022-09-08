d7c8172df87bed3624fdbff727aa21520d0875e1
Slavitt <112398002+Slavitt@users.noreply.github.com>
2022-09-08 08:40:41 -0400

    test

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,16 +1,18 @@
-#instructions: print out "Hello World!"
-#create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
-#create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
-
-
+# instructions: print out "Hello World!"
+# create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
+# create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
 
 def main():
 
 
+    print("Hello World!")
 
-    
-    
-    
-    
-    
+    favColor = "blue"
+    print("My favorite color is ", favColor)
+
+    birthDay = 23
+    birthMonth = 11
+    birthYear = 2003
+    print("My birthday is ", birthMonth, "/", birthDay, "/", birthYear)
+
 main()
--- Assignment 2/Debug.py
+++ Assignment 2/Debug.py
@@ -1,52 +1,41 @@
-#instructions: Something about these if statements aren't giving the desired effect. Look at them and see how to fix them. (Run them yourself and see what the output is!)
-#also, we have some input practice!
+# instructions: Something about these if statements aren't giving the desired effect. Look at them and see how to fix them.
+# (Run them yourself and see what the output is!)
+
 
 def main():
-    
-    #this is a nice solid working one!
-    name = input("your name is? ")
-    print("Hello, ", name)
-
-    #this isn't printing anything :( so sad. what's wrong with her? Why she not printing out?
-    color = input("what's your favorite color? ")
-    
-    #I thought i did this right :( can you fix it for me?
-    age = input("how old are you? ")
-    print("You are " + age + " years old")
-	
-    #Start with this one! We have a compilation error :(
-    #Side note: you can put these statements in parentheses or not. it's up to you.
-    if (5 > 3)
+    # Start with this one! We have a compilation error :(
+    # Side note: you can put these statements in parentheses or not. it's up to you.
+    if (5 > 3):
         print("5 is greater than 3")
-    
-    #There are multiple correct answers and adjustments to this one 
+
+    # There are multiple correct answers and adjustments to this one
     isFive = 5
     if isFive > 5:
         print("isFive is greater than 5")
-    else:
+    elif (isFive < 5):
         print("isFive is less than 5")
+    else:
+        print("isFive is equal to 5")
 
-    #more multiple correct answers. Similar to the first. Adjust as perceived 
+    # more multiple correct answers. Similar to the first. Adjust as perceived
     toCheck = 4
     if toCheck > 5:
         print(toCheck, "is greater than 5.")
     elif toCheck < 3:
-	    print(toCheck)
-    else:
+        print(toCheck)
+    elif toCheck == 3:
         print(toCheck, "is 3")
 
-    #Is it really printing the BEST option though? Rearrange these as you see fit
+    # Is it really printing the BEST option though? Rearrange these as you see fit
     toCheck = 5
-    if toCheck < 6:
-        print("toCheck is less than 6")
+    if toCheck == 5:   # originally the last statement
+        print("toCheck is 5")
     elif toCheck > 7:
         print("toCheck is greater than 7")
-    elif toCheck == 5:
-        print("toCheck is 5")
+    elif toCheck < 6:
+        print("toCheck is less than 6")
     else:
         print("toCheck is invalid")
-    
-    
-    
-    
+
+
 main()
