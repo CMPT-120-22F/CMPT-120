6767372ed8e8ea8feddbaa2f7adb7f55a1f3301f
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-09-14 21:22:58 -0400

    Update Debug.py

--- Assignment 2.5/Debug.py
+++ Assignment 2.5/Debug.py
@@ -1,1 +1,37 @@
-#to be filled out LOL
+#not done no touchy
+
+def main():
+
+    #that's not right... how can we make sure it also includes equals 5?
+    temp = 5
+    if temp > 5:
+        print("temp is greater than or equal to 5")
+        
+    
+    #enter your name here
+    name = ""
+    '''in this if/elif/else, add 4 mispellings of your name for if/elif comparisons, and then have your last elif be elif name = "your properly spelled name" '''
+    if name == "mispelling here":
+        print("That's not right!")
+    elif name == "another mispelling":
+        print("That's not right!")
+    #etc etc, the else can be whatever you want
+    
+    
+    #we're gonna check if a user input number is even
+    #pick any number
+    even = input(int("Enter a number to find out if its even or odd"))
+    even = int(even)
+    #what do we replace the question marks with?
+    if even ? ? == ?:
+        #what would be appropriate in these print statements?
+        print("")
+    else:
+        print("")
+  
+  
+main()
+    
+    
+
+    
