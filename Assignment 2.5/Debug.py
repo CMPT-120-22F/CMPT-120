f231f391383e93e7fb0cacdd7b5b78c6d37e5826
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-09-18 11:37:41 -0400

    Update Debug.py

--- Assignment 2.5/Debug.py
+++ Assignment 2.5/Debug.py
@@ -30,6 +30,21 @@ def main():
         print("")
   
   
+    #i'm trying to do math with the numbers 2 and 4, but it's getting 3 and 5... why?
+    numbers = [1,2,3,4,5]
+    print(numbers[2])
+    print(numbers[4])
+    print(numbers[4] / numbers[2])
+    print(numbers[4] * numbers[2])
+    
+    #again, why is it getting 7 and 20, I wanted 3 and 29!
+    numbers2 = [465,3,30,7,29,20,82,13,5]
+    print(numbers2[3])
+    print(numbers2[5])
+    
+    #Here's a fun one: This is a list of everyone's name. Find where yours is and print the index of your name
+    students = ["Achorn, Cameron", "Brown, Evan", "Catalano, Alexander", "Cianfoni, Alexander", "Delorey, Tyler","Edmonds, Jonah","Elliott, Dustin","Faix, Joseph","Fleischman, Connor","Fuerte, Caroline","Gidos, Hannah","Lavitt, Samuel","Lichstein, Harris","Longo, Nicholas","Martinez, David","Muggeri, Mattia","Munger, Ryan","Nealon, Ryan","Paulus, Natalie","Penn, Riley","Potenza, Amanda","Prochet, Carlisl","Putkaradze, Saba","Quayson, Eugene","Rietti, Cristina","Seeley, Shane","Sweeney, Quinn","Tata, Mathew","Taylor, Julia","Tuozzo, Michael"]
+    print(students[?])
 main()
     
     
