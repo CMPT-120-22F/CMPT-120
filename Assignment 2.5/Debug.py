c85e66bc329eaa99ad60e79573e4725f4c84dbf0
Slavitt <112398002+Slavitt@users.noreply.github.com>
2022-09-21 17:26:56 -0400

    assignment 2.5

--- Assignment 2.5/Debug.py
+++ Assignment 2.5/Debug.py
@@ -3,17 +3,17 @@ def main():
 
     #that's not right... how can we make sure it also includes equals 5?
     temp = 5
-    if temp > 5:
+    if temp >= 5:
         print("temp is greater than or equal to 5")
         
     
     #enter your name here
     name = ""
     '''in this if/elif/else, add 4 mispellings of your name for if/elif comparisons, and then have your last elif be elif name = "your properly spelled name" '''
-    if name == "mispelling here":
+    if name == "Samual":
         print("That's not right!")
-    elif name == "another mispelling":
-        print("That's not right!")
+    elif name == "Sami":
+        print("dude it's not that hard")
     #etc etc, the else can be whatever you want
     
     
@@ -22,14 +22,15 @@ def main():
     even = input(int("Enter a number to find out if its even or odd"))
     even = int(even)
     #what do we replace the question marks with?
-    if even ? ? == ?:
+    if even % 2 == 0:
         #what would be appropriate in these print statements?
-        print("")
+        print("This number is even!")
     else:
-        print("")
+        print("It was literally a 50% chance")
   
   
     #i'm trying to do math with the numbers 2 and 4, but it's getting 3 and 5... why?
+    #Arrays start on spot zero and not spot one
     numbers = [1,2,3,4,5]
     print(numbers[2])
     print(numbers[4])
@@ -43,7 +44,7 @@ def main():
     
     #Here's a fun one: This is a list of everyone's name. Find where yours is and print the index of your name
     students = ["Achorn, Cameron", "Brown, Evan", "Catalano, Alexander", "Cianfoni, Alexander", "Delorey, Tyler","Edmonds, Jonah","Elliott, Dustin","Faix, Joseph","Fleischman, Connor","Fuerte, Caroline","Gidos, Hannah","Lavitt, Samuel","Lichstein, Harris","Longo, Nicholas","Martinez, David","Muggeri, Mattia","Munger, Ryan","Nealon, Ryan","Paulus, Natalie","Penn, Riley","Potenza, Amanda","Prochet, Carlisl","Putkaradze, Saba","Quayson, Eugene","Rietti, Cristina","Seeley, Shane","Sweeney, Quinn","Tata, Mathew","Taylor, Julia","Tuozzo, Michael"]
-    print(students[?])
+    print(students[20])
 main()
     
     
