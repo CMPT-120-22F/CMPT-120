74f99c8351fb762121236f067376543449f28675
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-10-02 08:32:18 -0400

    Update Debug.py

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -1,15 +1,38 @@
 #this file is incomplete, do not complete ahead of time
-def main():
-    var1= 10
-    var2= 20
-    #What do we put in here to make it work?
-    addition()
-
-
-
+def printHello():
+    print("Hello")
+    
+def printName(x):
+    print(x)
+    
 def addition(x, y):
     #add x and y together and return them
     return
+
+def smaller(i, j):
+    #if i is smaller than j, return i
+    #if j is smaller than i, return j
+    #if they're even, return 0
+
+def main():
+    
+    #call the printHello function here
+    
+    #call printName and give it the parameter of your name
+    
+    var1= 10
+    var2= 20
+    #What do we put in here to make it work?
+    print(addition())
+    
+    num1 = int(input("Enter number 1"))
+    num2 = int(input("Enter number 2"))
+    #what go here?
+    print(smaller())
+
+
+
+
 
 
 main()
