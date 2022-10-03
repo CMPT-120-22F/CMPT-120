96544e9f7f2fe53e0b715cb3a72d97fd25e98e7f
Slavitt <112398002+Slavitt@users.noreply.github.com>
2022-10-03 08:01:13 -0400

    Update 9-28 Classwork.py

--- Classworks/9-28 Classwork.py
+++ Classworks/9-28 Classwork.py
@@ -9,3 +9,17 @@ i = 0
 while i < 10:
     print(i + 1)
     i = i + 1
+
+backRange = int(input("PICK A NUMBER "))
+sum = 0
+for i in range(1, backRange + 1):
+    sum = sum + i
+print(sum)
+
+list1 = [2, 3.456, "example", True]
+list2 = []
+
+for i in range(0, len(list1)):
+    list2.append(type(list1[i]))
+
+print(list2)
