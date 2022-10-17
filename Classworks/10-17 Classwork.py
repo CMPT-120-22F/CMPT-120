95cd809fa56ae4dda096217316d4af004c95f012
Slavitt <112398002+Slavitt@users.noreply.github.com>
2022-10-17 14:55:09 -0400

    10/17

---
+++ Classworks/10-17 Classwork.py
@@ -0,0 +1,13 @@
+def thisIsAFunction(x):
+    if x > 10:
+        print("big number")
+    elif x < 10:
+        print("small number")
+    elif x == 10:
+        print("medium number")
+
+def main():
+    thisIsAFunction(30)
+
+
+main()
