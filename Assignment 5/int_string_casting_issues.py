4476e0cd2d48b420e64cd6283759889f1510558c
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-10-22 13:43:31 -0400

    Create int_string_casting_issues.py

---
+++ Assignment 5/int_string_casting_issues.py
@@ -0,0 +1,10 @@
+'''
+mkay so
+I talked a lot about helping but not fixing the problem
+In this code, if you run this and enter not an integer, it throws an error, as we're aware, as you solved this problem in the debug file
+can you google a way to help or even fix this problem that isn't the way we were doing it before?
+'''
+def main():
+    intInput = int(input("Enter an int (but enter a string to see the error)"))
+    
+main()
