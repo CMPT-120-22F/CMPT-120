17720d3bdd73d717b37e195ee80d34023c9b93c2
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-10-22 13:40:29 -0400

    Update Debug.py

--- Assignment 5/Debug.py
+++ Assignment 5/Debug.py
@@ -1,1 +1,26 @@
-#
+def main():
+    #good working example!
+    stringInput = input("enter a string")
+    if stringInput.isalpha():
+        print("string!")
+    else:
+        print("not string :(")
+        
+    #can you google and find what function you should use to check if it's numeric (an int?)?
+    intInput = input("enter an int")
+    if intInput.?:
+        print("int!")
+    else:
+        print("not int :(")
+    
+    #what about if it's both letters and numbers?
+    alphIntInput = input("Enter letters and numbers")
+    if alphIntInput.?:
+        print("Letters and numbers!")
+    else:
+        print("weird characters :(")
+        
+        
+    #do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
+    
+main()
