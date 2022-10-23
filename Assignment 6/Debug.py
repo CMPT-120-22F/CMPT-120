0755abcb6dd70f43d9017d9bcc7f2f2a7659486d
Slavitt <112398002+Slavitt@users.noreply.github.com>
2022-10-23 14:37:37 -0400

    Update Debug.py

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -12,21 +12,25 @@ class Employee:
 class Cake:
     #can you fill out the rest of this for me? im dumb
     #the cake needs to have the cake flavor and cake frosting stored
+    def __init__(self, flavor, frosting):
+        self.flavor = flavor
+        self.frosting = frosting
         
 def main():
     #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
-    newDog = Dog(? , ?)
-    print(dog1.name, dog1.age)
+    newDog = Dog("Fozzie" , 10)
+    print("My dog", newDog.name, "is", newDog.age, "years old")
     
     #and what about a new employee
-    newEmployee =
+    newEmployee = Employee("Melissa Chodziutko", 42069, "Math + CS")
     #how would we print out each of the variables from newEmployee?
-    print()
+    print(newEmployee.name, newEmployee.idNumber, newEmployee.department)
     
     #now create and print out a cake you make
-    
-    
+    myCake = Cake("vanilla", "strawberry")
+    print(myCake.flavor, "cake with", myCake.frosting, "frosting")
     
     #and now create another cake and print it out
-
+    notMyCake = Cake("chocolate ice cream", "vanilla")
+    print(notMyCake.flavor, "cake with", notMyCake.frosting, "frosting")
 main()
