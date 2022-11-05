a6cb92410de7ae68c03c25668a4a426838a16463
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-11-05 09:16:32 -0400

    Update Debug.py

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -12,7 +12,29 @@ class Employee:
 class Cake:
     #can you fill out the rest of this for me? im dumb
     #the cake needs to have the cake flavor and cake frosting stored
+
+class Cat:
+    def __init__(self, name, age, fur_length):
+        self.name = name
+        self.age = age
+        self.fur_length = fur_length
         
+        
+    def breedGuess(self):
+        if self.fur_length == "long":
+            return("Domestic Longhair")
+        else:
+            return("Domestic Shorthair")
+            
+class Car:
+    def __init__(self, model, year, color):
+        self.model = model
+        self.year = year
+        self.color = color
+        
+    #create your own function! what do you want it to do?
+    
+   
 def main():
     #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
     newDog = Dog(? , ?)
@@ -28,5 +50,18 @@ def main():
     
     
     #and now create another cake and print it out
+    
+    
+    
+    #create a cat!
+    cat1 = Cat()
+    #create another cat!
+    
+    #What would we put here to print out the result of breedGuess for cat1?
+    print(cat1.?)
+    
+    #create a car!
+    
+    #Now print out the function you made for car :)
 
 main()
