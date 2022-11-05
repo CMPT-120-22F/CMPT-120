480359bbc07563c000e2b343db8ed723bc8965ca
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2022-11-05 09:26:15 -0400

    Create student.py

---
+++ Assignment 6/student.py
@@ -0,0 +1,37 @@
+#oo scary
+'''
+Instructions:
+We're now experts at classes, right?
+Yeah?
+mkay so do me a favor
+Create the class student
+every student has these traits:
+name
+student id (you can pick this number arbitrarily)
+year (f/s/j/s)
+major
+gpa
+
+create a function to see if the student is eligible for the honors program
+to be eligible they need to have a gpa above 3.5
+return true if they can and false if they cant, and print it out
+
+create a function because this college is a wacky one- every day they generate a student id and if the student id matches a student, that student gets free food that day. 
+1. generate a random number the length of however long you choose to make the id number
+2. compare if the random number matches your student's id
+3. if it matches print out "winner! student (name) gets free lunch!"
+4. if not, print "Loser!"
+(disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
+'''
+
+class student:
+    
+    
+    
+    
+    
+    
+def main():
+    
+    
+main()
