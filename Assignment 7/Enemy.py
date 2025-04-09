81f5491528ef3177df6258d97b83f3d852c94ea0
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2025-04-09 09:19:34 -0400

    Create Enemy.py

---
+++ Assignment 7/Enemy.py
@@ -0,0 +1,8 @@
+#Empty file!
+'''
+Create an enemy class with health.
+create a goblin child class with a function called "attack" that says "the gobllin gobbles"
+create a wizard child class that also has a 'magicType' variable (wizards use different types of magic!) and their attack function should tell the user what type of magic they've been hit with!
+
+(there should be no main, unless you want to test these classes- i'm not expecting you to use them, I would just like to see correctly build classes and functions!)
+'''
